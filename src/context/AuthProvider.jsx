14d648a57a7dafcb66be227c6c0d8d5b620b93a5
valenoirs <luckyvw.general@gmail.com>
2022-06-13 15:07:54 +0800

    development: redirect

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,18 +1,23 @@
 import { useState } from "react"
 import AuthContext from "./AuthContext"
 import fakeAuth from "../utils/fakeAuth"
+import { useNavigate } from "react-router-dom"
 
 const AuthProvider = ({children}) => {
     const [token, setToken] = useState(null)
+
+    const navigate = useNavigate()
 
     const handleLogin = async () => {
         const token = await fakeAuth()
         console.log(token)
         setToken(token)
+        navigate('/dashboard')
       }
-    
+      
       const handleLogout = () => {
         setToken(null)
+        navigate('/')
       }
 
       const value = {
