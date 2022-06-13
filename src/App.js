2cdb3ed5004a129473b804e0c584362031eedc7c
valenoirs <luckyvw.general@gmail.com>
2022-06-13 14:09:24 +0800

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+/* eslint-disable */
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+
+function App() {
+  return (
+    <main>
+      <h1>Learning React Auth</h1>
+    </main>
+  );
+}
+
+export default App;
